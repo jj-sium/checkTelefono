50ed4c708df1e4938b90cd92cdfe12865675b9a7
Johan Valentino Ghinelli <116793076+jj-sium@users.noreply.github.com>
2023-09-28 12:25:40 +0200

    Add files via upload

---
+++ checkTelefono.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public static class Telefono
+{
+
+    public static string Check(string[] input)
+    {
+        for(int b=0; b>input.Lenght; b++)
+        {
+            string str = input[b];
+            if(str[b]=="+39")
+            {
+                
+            }
+            if(str[b]=="0039")
+            {
+                
+            }
+            if(str[b]=="3")
+            {
+                
+            }
+            return str;
+        }
+        return "";
+    }
+}
