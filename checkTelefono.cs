3af86c457fe143868578655a4e7d45f40762cc17
Johan Valentino Ghinelli <116793076+jj-sium@users.noreply.github.com>
2023-09-30 15:38:14 +0200

    Update checkTelefono.cs

--- checkTelefono.cs
+++ checkTelefono.cs
@@ -4,24 +4,30 @@ using System.Collections.Generic;
 public static class Telefono
 {
 
-    public static string Check(string[] input)
+    public static string Check(string[] inputStr)
     {
-        for(int b=0; b>input.Lenght; b++)
+        foreach(string input in inputStr)
         {
-            string str = input[b];
-            if(str[b]=="+39")
+            if(input.StartsWith("0039") )
             {
-                
+                if(input.Length==14)
+                    return input;
             }
-            if(str[b]=="0039")
+            
+            else if(input.StarstWith("+39") )
             {
-                
+                if(input.Length==13)
+                    return input;
             }
-            if(str[b]=="3")
+            
+            else if(input.StartsWith("3") )
             {
-                
+                Int64 valore = 0;
+                bool eUnNumero = Int64.TryParse(input, out valore);
+                if(eUnNumero)
+                    if(input.Length == 10)
+                        return input;
             }
-            return str;
         }
         return "";
     }
